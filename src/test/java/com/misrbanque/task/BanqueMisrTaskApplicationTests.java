bd30c78fa21bf4b34d0a7598cd12e1c03b70c043
Baraheem <ibrahim.ahmed@gizasystems.com>
2022-10-23 15:44:27 +0200

    First commit

---
+++ src/test/java/com/misrbanque/task/BanqueMisrTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misrbanque.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueMisrTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
